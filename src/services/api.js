40cab1c30d659a4059f20d0eaef1581c4eed8cd1
andrebury <andre.bury@live.com>
2020-04-03 12:50:05 -0300

    variaveis de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import .env from './env';
+
 
 const api = axios.create({
-	baseURL: 'https://acompanhamento-projeto.herokuapp.com'
+	//baseURL: 'https://acompanhamento-projeto.herokuapp.com'
+	baseURL: process.env.REACT_URL_API_ADDRESS
 });
 
 export default api;
