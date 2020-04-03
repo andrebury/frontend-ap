f3f5a8b0a1c58c3d77a26dc0edb55ac7047a109e
andrebury <andre.bury@live.com>
2020-04-03 13:50:12 -0300

    mais env-vars2

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-
 const api = axios.create({
-	baseURL: 'https://acompanhamento-projeto.herokuapp.com'
-	//baseURL: process.env.REACT_URL_API_ADDRESS
+	//baseURL: 'https://acompanhamento-projeto.herokuapp.com'
+	baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
